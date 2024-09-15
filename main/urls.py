444278674074029593fcf887be4ea1aadfd25ed6
MRBOX <olamigokeadebayo90@gmail.com>
2024-09-16 00:16:09 +0100

    Created the index page and the signup page

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from main import views
 
-app_name = 'main'
+# app_name = 'main'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register')
 ]
